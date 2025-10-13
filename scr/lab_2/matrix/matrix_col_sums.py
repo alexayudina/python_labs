884235955dc334367f76739e9afc697066c583ca
alexayudina <alexa.yudina07@gmail.com>
2025-10-13 23:26:04 +0300

    Add files via upload

---
+++ scr/lab_2/matrix/matrix_col_sums.py
@@ -0,0 +1,18 @@
+def col_sums(mat):
+    if not mat:
+        return [] 
+    n = len(mat[0])
+    for row in mat:
+        if len(row) != n:
+            return "ValueError"  
+    res = []  
+    for j in range(n):  
+        k = 0  
+        for i in range(len(mat)):  
+            k += mat[i][j] 
+        res.append(k)  
+    return res 
+print(col_sums([[1, 2, 3], [4, 5, 6]]))
+print(col_sums([[-1, 1], [10, -10]]))
+print(col_sums([[0, 0], [0, 0]]))
+print(col_sums([[1, 2], [3]]))
---
+++ scr/lab_2/matrix/matrix_transpose.py
@@ -0,0 +1,20 @@
+def transpose(mat: list[list[float | int]]) -> list[list]:
+    if not mat:
+        return []
+    n = len(mat[0]) 
+    for row in mat:
+        if len(row) != n:
+            return "ValueError"  
+    res = [] 
+    for j in range(n): 
+        new_row = []  
+        for i in range(len(mat)): 
+            new_row.append(mat[i][j])  
+        res.append(new_row)  
+    return res  
+
+print(transpose([[1, 2, 3]]))
+print(transpose([[1], [2], [3]]))
+print(transpose([[1, 2], [3, 4]]))
+print(transpose([]))
+print(transpose([[1, 2], [3]]))
